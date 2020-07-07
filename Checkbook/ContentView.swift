c02980ea727e916a0f3de040bd347f57db7531b8
Dominic <32912858+iAmericanBoy@users.noreply.github.com>
2020-07-07 19:43:07 -0400

    Initial Commit

---
+++ Checkbook/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Checkbook
+//
+//  Created by Dominic Lanzillotta on 7/7/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
